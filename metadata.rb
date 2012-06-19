5193b9470e83d50f05184fb5b8f3dc5b62087709
Cosmin Stejerean <cosmin@offbytwo.com>
2012-06-18 18:20:42 -0700

    stop depending on yum if it is only used on rehad and centos 5.x

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,6 @@ description       "Installs Python, pip and virtualenv. Includes LWRPs for manag
 version           "1.0.6"
 
 depends           "build-essential"
-depends           "yum"
 
 recipe "python", "Installs python, pip, and virtualenv"
 recipe "python::package", "Installs python using packages."
