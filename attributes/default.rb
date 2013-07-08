e943429c986c9078e4c751ca597bc7172a6a92d7
Shawn Neal <sneal@sneal.net>
2013-07-08 16:07:42 -0700

    [COOK-2809] Added Windows support to the Python cookbook
    The package recipe will now include the windows recipe if run on platform windows. This will install the standard windows python msi for the current OS, either x64 or x86.

--- attributes/default.rb
+++ attributes/default.rb
@@ -35,5 +35,14 @@ default['python']['binary'] = "#{python['prefix_dir']}/bin/python"
 
 default['python']['url'] = 'http://www.python.org/ftp/python'
 default['python']['version'] = '2.7.1'
-default['python']['checksum'] = '80e387bcf57eae8ce26726753584fd63e060ec11682d1145af921e85fd612292'
 default['python']['configure_options'] = %W{--prefix=#{python['prefix_dir']}}
+
+if platform?('windows')
+  if kernel['machine'] =~ /x86_64/
+    default['python']['checksum'] = '48ad736ec1bd74115df47f56c4ea770bc39dcde96a3f94270a549167e7f4dc65'
+  else
+    default['python']['checksum'] = '0af8bc05a5f9ed15df120150c6a6ddd0fc50d018e35b891cba22040e0404dfb6'
+  end
+else
+  default['python']['checksum'] = '80e387bcf57eae8ce26726753584fd63e060ec11682d1145af921e85fd612292'
+end
--- recipes/windows.rb
+++ recipes/windows.rb
@@ -1,9 +1,9 @@
 #
-# Author:: Seth Chisamore <schisamo@opscode.com>
+# Author:: Shawn Neal <sneal@daptiv.com>
 # Cookbook Name:: python
-# Recipe:: default
+# Recipe:: windows
 #
-# Copyright 2011, Opscode, Inc.
+# Copyright 2013, Opscode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,10 +18,22 @@
 # limitations under the License.
 #
 
-if platform?('windows')
-  include_recipe "python::package"
+checksum = node['python']['checksum']
+version = node['python']['version']
+
+if node['kernel']['machine'] =~ /x86_64/
+  msi_file_name = "python-#{version}.amd64.msi"
 else
-  include_recipe "python::#{node['python']['install_method']}"
-  include_recipe "python::pip"
-  include_recipe "python::virtualenv"
+  msi_file_name = "python-#{version}.msi"
+end
+
+package_name = "Python #{version}"
+url = "#{node['python']['url']}/#{version}/#{msi_file_name}"
+
+windows_package package_name do
+  action :install
+  source url
+  checksum checksum
+  options 'ALLUSERS=1' #Workaround for http://bugs.python.org/issue16188
+  installer_type :msi
 end
