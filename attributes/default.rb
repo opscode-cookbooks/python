97e744d2804a05e0422180b5a779333c2cb47871
Peer Allan <peer.allan@shopify.com>
2015-01-20 14:57:07 -0600

    Adds ability to override the installed version of PIP

--- attributes/default.rb
+++ attributes/default.rb
@@ -40,6 +40,8 @@ default['python']['configure_options'] = %W{--prefix=#{node['python']['prefix_di
 default['python']['make_options'] = %W{install}
 
 default['python']['pip_location'] = "#{node['python']['prefix_dir']}/bin/pip"
+default['python']['pip_version'] = nil
+
 default['python']['virtualenv_location'] = "#{node['python']['prefix_dir']}/bin/virtualenv"
 default['python']['setuptools_version'] = nil # defaults to latest
 default['python']['virtualenv_version'] = nil
