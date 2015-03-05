446c33269c2d7137e2fcfeac77bf0ff027b19034
phamvanhung <phamvanhung2e123@gmail.com>
2015-03-05 11:24:15 +0900

    wget cannot redirect http to https

--- attributes/default.rb
+++ attributes/default.rb
@@ -33,7 +33,7 @@ end
 
 default['python']['binary'] = "#{node['python']['prefix_dir']}/bin/python"
 
-default['python']['url'] = 'http://www.python.org/ftp/python'
+default['python']['url'] = 'https://www.python.org/ftp/python'
 default['python']['version'] = '2.7.7'
 default['python']['checksum'] = '7f49c0a6705ad89d925181e27d0aaa025ee4731ce0de64776c722216c3e66c42'
 default['python']['configure_options'] = %W{--prefix=#{node['python']['prefix_dir']}}
