69ee5f9e9d2d9e44c7e825739b5087e695d81dfa
Mike Fiedler <miketheman@gmail.com>
2016-01-20 10:08:01 -0500

    Use the version directly instead of piping
    Turns out the example shown was for `curl|python...`, not invoking
    directly. So we can use the same version identifer as if we were using
    `pip` command itself.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "noah@coderanger.net"
 license           "Apache 2.0"
 description       "Installs Python, pip and virtualenv. Includes LWRPs for managing Python packages with `pip` and `virtualenv` isolated Python environments."
 # This is our fork's version. It does not correspond to the upstream's cookbook at all.
-version           "1.4.10"
+version           "1.4.11"
 
 depends           "build-essential"
 depends           "yum-epel"
